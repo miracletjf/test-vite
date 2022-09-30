66fd6db2822a339abb5e02976474e016f16a0193
ATang <1398988546@qq.com>
2022-09-30 18:18:24 +0800

    feat: first commit 1. 初始化项目 2. 安装 vite 3. 配置环境 env

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { defineConfig, loadEnv } = require("vite")
+const path = require("path")
+
+module.exports = defineConfig(({ command, mode }) => {
+  console.log("command", command)
+  console.log("mode", mode)
+
+  const env = loadEnv(mode, path.resolve(__dirname, "./env"), "")
+  console.log("load env", env)
+  return {
+    envDir: path.resolve(__dirname, "./env"),
+    envPrefix: "OUT_"
+  }
+})
