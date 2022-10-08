1ca9501881347a2f22536e6ce12fc0e183e87055
portfolio <1398988546@qq.com>
2022-10-08 06:44:28 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-const { defineConfig, loadEnv } = require("vite")
-const path = require("path")
+const { defineConfig, loadEnv } = require("vite");
+const path = require("path");
+const { log } = require("console");
 
 module.exports = defineConfig(({ command, mode }) => {
-  console.log("command", command)
-  console.log("mode", mode)
+  console.log("command", command);
+  console.log("mode", mode);
+  console.log(111);
 
-  const env = loadEnv(mode, path.resolve(__dirname, "./env"), "")
-  console.log("load env", env)
+  const env = loadEnv(mode, path.resolve(__dirname, "./env"), "");
+  console.log("load env", env);
   return {
-    envDir: path.resolve(__dirname, "./env")
-  }
-})
+    envDir: path.resolve(__dirname, "./env"),
+  };
+});
